6b0fec10b7f29abcf9cf41d661e28f798b1911c0
JD Alexander Kemp <jdakemp@gmail.com>
2020-11-04 12:37:38 -0500

    bounce man project added with image

--- src/shared/projects.js
+++ src/shared/projects.js
@@ -5,43 +5,49 @@ export const PROJECTS =
             name:'Morrell Ministries',
             image: 'assets/images/MorrellMinstries.png',
             category: 'mains',
-            description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-            comments: [
+            description:'Sadey’s Place is a non-profit shelter to provide housing facility to women returning from\n' +
+            'prison to re-establish into community. The organization provides 90-day re-settlement program\n' +
+            'which will involve various stakeholders in the community including local religious groups,\n' +
+            'donors, and the government authorities. The main objective of the 90-day reentry program by\n' +
+                'Sadey’s Place is to help the women returning from prison reestablish into the community,',
+            Project: [
                 {
                     id: 0,
-                    rating: 5,
-                    comment: "Imagine all the eatables, living in conFusion!",
-                    author: "John Lemon",
-                    date: "2012-10-16T17:57:28.556094Z"
+                    comment: " Problem, Solution, Statement ",
                 },
                 {
                     id: 1,
-                    rating: 4,
-                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                    author: "Paul McVites",
-                    date: "2014-09-05T17:57:28.556094Z"
-                },
-                {
-                    id: 2,
-                    rating: 3,
-                    comment: "Eat it, just eat it!",
-                    author: "Michael Jaikishan",
-                    date: "2015-02-13T17:57:28.556094Z"
-                },
-                {
-                    id: 3,
-                    rating: 4,
-                    comment: "Ultimate, Reaching for the stars!",
-                    author: "Ringo Starry",
-                    date: "2013-12-02T17:57:28.556094Z"
-                },
-                {
-                    id: 4,
-                    rating: 2,
-                    comment: "It's your birthday, we're gonna party!",
-                    author: "25 Cent",
-                    date: "2011-12-02T17:57:28.556094Z"
+                    comment: " Jd Help my company ... We love the site and highly recommend him! ",
+                    author: " LaDonna Morrell CEO ",
+                    date: "2012-10-16T17:57:28.556094Z"
                 }
 
+
+
+    ]   },
+        {
+            id: 1,
+            name:'Bounce Man Game',
+            image: 'assets/images/BounceMan.png',
+            category: 'mains',
+            description: 'Bounce Man description',
+            Project: [
+                {
+                    id:0,
+                    comment: " Problem, Solution, Statement ",
+                },
+                {
+                    id: 1,
+                    comment: " Great Job on the assignment I like that you.... Great job! ",
+                    author: " Dr Simon Katan University Of London Instructor ",
+                    date: "2012-10-16T17:57:28.556094Z"
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: ' 23.5 Degrees Consulting ',
+            image: ''
         }
-    ]
+
+]
